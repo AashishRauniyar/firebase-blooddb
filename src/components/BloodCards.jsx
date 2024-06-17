0c7652c1b33f4a72dad325508b2b0759bad15ef9
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-06-17 23:58:24 +0545

    delete functionality added

--- src/components/BloodCards.jsx
+++ src/components/BloodCards.jsx
@@ -1,9 +1,23 @@
+import { deleteDoc, doc } from 'firebase/firestore'
 import React from 'react'
 import { HiOutlineUserCircle } from 'react-icons/hi'
 import { IoMdTrash } from 'react-icons/io'
 import { RiEditCircleLine } from 'react-icons/ri'
+import { db } from '../config/firebase'
 
 const BloodCards = ({blood}) => {
+
+    const deleteBlood = async (id) => {
+        try {
+            await deleteDoc(doc(db, "blood", id));
+            console.log("Blood deleted with ID: ", id);
+        } catch (e) {
+            console.error("Error deleting blood: ", e);
+        }
+    }
+
+
+
   return (
     <>
     <div key={blood.id} className="bg-slate-200 flex justify-between items-center rounded-lg p-3">
@@ -18,7 +32,7 @@ const BloodCards = ({blood}) => {
               </div>
               <div className="flex gap-1 ">
                 <RiEditCircleLine className="text-xl cursor-pointer" />
-                <IoMdTrash className="text-2xl text-orange-400 cursor-pointer" />
+                <IoMdTrash onClick={() => deleteBlood(blood.id)} className="text-2xl text-orange-400 cursor-pointer" />
               </div>
             </div>
     </>
