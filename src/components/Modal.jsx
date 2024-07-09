90d0132f41f4e2ef5c887263da55990eb95af624
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-07-09 12:32:03 +0545

    minor changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,23 +1,59 @@
-import React from 'react'
-import { createPortal } from 'react-dom'
-import { AiOutlineClose } from 'react-icons/ai'
+// import React from 'react'
+// import { createPortal } from 'react-dom'
+// import { AiOutlineClose } from 'react-icons/ai'
 
-const Modal = ({onClose,isOpen,children} ) => {
+// const Modal = ({onClose,isOpen,children} ) => {
+//   return createPortal(
+//     <>
+//     {isOpen && (
+//         <div onClick={onClose} className='grid  absolute top-0 z-40 backdrop-blur h-screen w-screen' >
+//         <div className='m-auto z-50 relative min-h-[200px] min-w-[80%] bg-orange-300 p-4'>
+//             <div className='flex justify-end' >
+//                 <AiOutlineClose onClick={onClose} className='text-2xl text-red-400 cursor-pointer ' />
+//             </div>
+//             {children}
+//         </div>
+        
+//         </div>
+//       )}
+//     </>
+//   , document.getElementById('modal-root'))
+// }
+
+// export default Modal
+import React from 'react';
+import { createPortal } from 'react-dom';
+import { AiOutlineClose } from 'react-icons/ai';
+
+const Modal = ({ onClose, isOpen, children }) => {
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
   return createPortal(
     <>
-    {isOpen && (
-        <div onClick={onClose} className='grid  absolute top-0 z-40 backdrop-blur h-screen w-screen' >
-        <div className='m-auto z-50 relative min-h-[200px] min-w-[80%] bg-orange-300 p-4'>
-            <div className='flex justify-end' >
-                <AiOutlineClose onClick={onClose} className='text-2xl text-red-400 cursor-pointer ' />
+      {isOpen && (
+        <div
+          onClick={onClose}
+          className='grid absolute top-0 z-40 backdrop-blur h-screen w-screen'
+        >
+          <div
+            onClick={stopPropagation}
+            className='m-auto z-50 relative min-h-[200px] min-w-[80%] bg-orange-300 p-4'
+          >
+            <div className='flex justify-end'>
+              <AiOutlineClose
+                onClick={onClose}
+                className='text-2xl text-red-400 cursor-pointer'
+              />
             </div>
             {children}
-        </div>
-        
+          </div>
         </div>
       )}
-    </>
-  , document.getElementById('modal-root'))
-}
+    </>,
+    document.getElementById('modal-root')
+  );
+};
 
-export default Modal
+export default Modal;
